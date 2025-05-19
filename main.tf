7128a173c40e57e5f8ae7981cd43eba40f85fbb3
RahulPanchal0110 <105359148+RahulPanchal0110@users.noreply.github.com>
2025-05-19 14:45:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,1 @@
 resource "null_resource" "one" {}
-
-policy "gcp-cis-7.10-kubernetes-ensure-basic-authentication-is-disabled-on-kubernetes-engine-clusters" {
-    source = "https://raw.githubusercontent.com/hashicorp/terraform-foundational-policies-library/master/cis/gcp/kubernetes/gcp-cis-7.10-kubernetes-ensure-basic-authentication-is-disabled-on-kubernetes-engine-clusters/gcp-cis-7.10-kubernetes-ensure-basic-authentication-is-disabled-on-kubernetes-engine-clusters.sentinel"
-    enforcement_level = "advisory"
-}
